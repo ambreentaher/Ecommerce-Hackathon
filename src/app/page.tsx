a154a2464b033d1ec730c016a1d3d7b711e3b553
AmbreenAbid <161235823+ambreentaher@users.noreply.github.com>
2024-12-16 09:10:11 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Announce from "@/components/announcement";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+     <Announce/>
+     <Header/>
+     <Hero/>
+    </div>
+  );
+}
